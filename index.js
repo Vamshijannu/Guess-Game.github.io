def69c84692c41bdb4003521b6ad30bb27e116e1
Vamshijannu <96859984+Vamshijannu@users.noreply.github.com>
2022-12-07 19:55:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+let computerGuess;
+let userGuess=[];
+
+let userGuessUpdate=document.getElementById("textOutput");
+let userNumberUpdate=document.getElementById("inputBox");
+let audio= new Audio("Gameclick.wav");
+function init() {
+    computerGuess = Math.floor(Math.random() * 100);
+    document.getElementById("newGameButton").style.display = "none";
+    document.getElementById("gameArea").style.display = "none";
+}
+// reload the page
+const newGameBegin=()=>{
+    audio.play();
+    window.location.reload();
+}
+
+const startGame = () =>{
+    document.getElementById("gameArea").style.display = "block";
+    document.getElementById("welcomeScreen").style.display="none";
+}
+const startNewGame = () =>{
+    document.getElementById("newGameButton").style.display = "inline";
+    userNumberUpdate.setAttribute("disabled",true)  }
+const compareGuess= () =>{
+    audio.play();
+    const usernumber = Number(document.getElementById("inputBox").value);
+    userGuess = [...userGuess,usernumber];
+    document.getElementById("guesses").innerHTML=userGuess;
+
+     // cheching guess low or high
+     if(userGuess.length < maxGuess){
+     if (usernumber > computerGuess){
+        userGuessUpdate.innerHTML = "Your guess is High 😟";
+        userNumberUpdate.value="";
+   }
+    else if (usernumber < computerGuess){
+       userGuessUpdate.innerHTML = "Your guess is Low 😓";
+       userNumberUpdate.value="";
+    }
+    else{
+       userGuessUpdate.innerHTML = "HURRAY it's Correct 😎";
+       userNumberUpdate.value="";
+       startNewGame();
+   }
+}else{
+    if(usernumber>computerGuess){
+        userGuessUpdate.innerHTML = `You Loose !! Correct Number was ${computerGuess}`;
+        userNumberUpdate.value="";
+        startNewGame();
+   }
+    else if(usernumber<computerGuess){
+       userGuessUpdate.innerHTML = `You Loose !! Correct Number was ${computerGuess}`;
+       userNumberUpdate.value="";
+       startNewGame();
+    }
+    else{
+       userGuessUpdate.innerHTML = `HURRAY it's Correct 😎`;
+       userNumberUpdate.value="";
+       startNewGame();
+   }
+   document.getElementById("attempts").innerHTML=userGuess.length;
+
+}
+   
+
+}
+
+const easyMode = () =>{
+    audio.play();
+    maxGuess=10;
+    startGame();
+}
+const hardMode = () =>{
+    audio.play();
+    maxGuess=5;
+    startGame();
+}
